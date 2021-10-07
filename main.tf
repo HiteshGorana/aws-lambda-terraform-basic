3084be67e05fec4c77c6d23dd8426d855065c523
Dimension <to.hitesh.gorana@gmail.com>
2021-10-07 10:52:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,15 @@ terraform {
   }
 }
 variable "aws_region" {
-  type = string
-  default = "us-east-1"
+  type = map
+  default = {
+    dev = "us-east-1"
+    prod = "eu-west-2"
+  }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = var.aws_region[terraform.workspace]
 }
 
 data "archive_file" "main_zip" {
@@ -30,7 +33,7 @@ data "archive_file" "main_zip" {
 
 resource "aws_lambda_function" "lambda_v" {
   filename = "main.zip"
-  function_name = "lambda_v_test"
+  function_name = "lambda_v_test_${terraform.workspace}"
   role          = aws_iam_role.lambda_v_role
   handler = "main.lambda_handler"
   runtime = "python3.8"
@@ -55,4 +58,21 @@ resource "aws_iam_role" "lambda_v_role" {
   ]
 }
 EOF
+}
+
+resource "aws_sqs_queue" "main_queue" {
+  name = "my-main-queue_${terraform.workspace}"
+  delay_seconds = 30
+  max_message_size = 262144
+}
+
+resource "aws_sqs_queue" "dlq_queue" {
+  name             = "my-dlq-queue_${terraform.workspace}"
+  delay_seconds    = 30
+  max_message_size = 262144
+}
+
+resource "aws_lambda_event_source_mapping" "sqs-lambda-trigger" {
+  event_source_arn = aws_sqs_queue.main_queue.arn
+  function_name = aws_lambda_function.lambda_v.arn
 }
